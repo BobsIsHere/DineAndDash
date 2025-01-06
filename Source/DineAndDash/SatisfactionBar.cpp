e960c20fdd0855678bb3cfc0f94cabfc51bd2b59
BobsIsHere <maritte.kindt@student.howest.be>
2025-01-06 17:39:20 +0100

    Made Basic C++ Satisfaction Class

--- Source/DineAndDash/SatisfactionBar.cpp
+++ Source/DineAndDash/SatisfactionBar.cpp
@@ -2,4 +2,56 @@
 
 
 #include "SatisfactionBar.h"
+#include "Components/ProgressBar.h"
+#include "Components/HorizontalBox.h"
+#include "Components/Image.h"
 
+void USatisfactionBar::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (StarBox)
+	{
+		for (UWidget* Child : StarBox->GetAllChildren())
+		{
+			UImage* StarImage = Cast<UImage>(Child);
+
+			if (StarImage)
+			{
+				StarImages.Add(StarImage);
+			}
+		}
+	}
+
+	CurrentThreshold = 0;
+	SatisfactionThreshold = 30;
+
+	YellowColor = FLinearColor(1, 0.5, 0, 1);
+	WhiteColor = FLinearColor(1, 1, 1, 1);  
+}
+
+void USatisfactionBar::UpdateSatisfaction(int32 Score)
+{
+	if (!ProgressBar || StarImages.Num() == 0)
+	{
+		return;
+	}
+
+	// Update Satisfaction Bar
+	ProgressBar->SetPercent(static_cast<float>(Score / SatisfactionThreshold));
+
+	if (Score >= CurrentThreshold + SatisfactionThreshold)
+	{
+		CurrentThreshold += SatisfactionThreshold;
+
+		if (CurrentStarIndex < StarImages.Num())
+		{
+			if (StarImages[CurrentStarIndex]->GetColorAndOpacity() == WhiteColor)  
+			{
+				StarImages[CurrentStarIndex]->SetColorAndOpacity(YellowColor); 
+
+				ProgressBar->SetPercent(0);
+			}
+		}
+	}
+}
--- Source/DineAndDash/SatisfactionBar.h
+++ Source/DineAndDash/SatisfactionBar.h
@@ -6,6 +6,10 @@
 #include "Blueprint/UserWidget.h"
 #include "SatisfactionBar.generated.h"
 
+class UProgressBar;
+class UHorizontalBox;
+class UImage;
+
 /**
  * 
  */
@@ -14,4 +18,36 @@ class DINEANDDASH_API USatisfactionBar : public UUserWidget
 {
 	GENERATED_BODY()
 	
+protected:
+	// Progress Bar reference
+	UPROPERTY(meta = (BindWidget))
+	UProgressBar* ProgressBar; 
+
+	// Horizontal Box containing stars
+	UPROPERTY(meta = (BindWidget))
+	UHorizontalBox* StarBox;
+
+	// Star Images
+	TArray<UImage*> StarImages;
+
+	// Color Variables
+	FLinearColor YellowColor;
+	FLinearColor WhiteColor; 
+
+	// Current star index
+	int32 CurrentStarIndex;
+
+	// Threshold for satisfaction
+	float SatisfactionThreshold;
+
+	// Current theshold value
+	float CurrentThreshold;
+
+	// Override NativeConstruct
+	virtual void NativeConstruct() override;
+
+public:
+	// Update satisfaction bar
+	UFUNCTION(BlueprintCallable, Category = "Satisfaction")
+	void UpdateSatisfaction(int32 Score);
 };
